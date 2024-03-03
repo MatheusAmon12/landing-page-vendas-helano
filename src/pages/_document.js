56fb07f8fb73ad1f30bff1da38066c9aa67ad9cf
Matheus Amon <amonmatheus757@gmail.com>
2024-03-03 16:26:24 -0300

    Adicionando noscript do gtm

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -47,6 +47,10 @@ class MyDocument extends Document {
           `}} />
         </Head>
         <body>
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5FK3GFP"
+            height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
