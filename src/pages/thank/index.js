7fcbf8cec9dcd67c2e6613c3926187adee0bb0f5
Matheus Amon <amonmatheus757@gmail.com>
2024-03-03 15:50:51 -0300

    Adicionando página de obrigado

---
+++ src/pages/thank/index.js
@@ -0,0 +1,98 @@
+import { Container, Typography } from '@mui/material'
+import { makeStyles } from 'tss-react/mui'
+
+import Button from '@/components/Button'
+import TemplateDefault from '@/templates/Default'
+
+const useStyles = makeStyles()((theme) => {
+    return{
+        container: {
+            height: '944px',
+            backgroundImage: 'url(/images/thankPage_bg.png)',
+            backgroundPosition: 'center center',
+            [theme.breakpoints.down('sm')]: {
+                height: '1289px',
+                backgroundImage: 'url(/images/thankPage_bg_sm.png)'
+            }
+        },
+        common_width: {
+            width: '700px',
+            [theme.breakpoints.down('sm')]: {
+                width: '350px'
+            }
+        },
+        common_padding: {
+            padding: '80px 0'
+        },
+        margin_24: {
+            marginBottom: '24px'
+        },
+        margin_40: {
+            marginBottom: '40px'
+        },
+        thank: {
+            fontSize: '38px',
+            lineHeight: '48px',
+            [theme.breakpoints.down('sm')]: {
+                fontSize: '32px',
+                lineHeight: '42px'
+            }
+        },
+        text: {
+            fontSize: '24px',
+            lineHeight: '34px',
+            [theme.breakpoints.down('sm')]: {
+                fontSize: '16px',
+                lineHeight: '26px'
+            }
+        }
+    }
+}) 
+
+const Thank = () => {
+    const { classes } = useStyles()
+
+    return(
+        <TemplateDefault>
+            <Container maxWidth={'100vw'} className={classes.container}>
+                <Container maxWidth={'md'} className={classes.common_padding}>
+                    <Typography
+                        variant={'h1'}
+                        color={'#75DBCD'}
+                        className={`${classes.common_width} ${classes.thank} ${classes.margin_24}`}
+                    >
+                        OBRIGADO POR DAR O PRIMEIRO PASSO PARA UMA JORNADA TRANSFORMADORA
+                    </Typography>
+
+                    <Typography
+                        lineHeight={'26px'}
+                        color={'#EBE5E5'}
+                        className={`${classes.common_width} ${classes.text} ${classes.margin_24}`}
+                    >
+                        Neste curso, vamos explorar juntos a importância de olhar para dentro de si, de se conhecer profundamente, de se aceitar com amor e compaixão, e de transformar essas descobertas em poderosas ferramentas de crescimento pessoal.
+                        <br/><br/>
+                        Durante nossa jornada juntos, vamos mergulhar fundo nas camadas da sua essência, descobrindo seus talentos, suas paixões e seus propósitos mais profundos. Vamos desafiar crenças limitantes, enfrentar medos e abrir espaço para o florescimento de uma nova versão de si mesma - uma versão mais autêntica, mais confiante e mais realizada.
+                    </Typography>
+
+                    <Typography
+                        color={'#75DBCD'}
+                        className={`${classes.common_width} ${classes.text} ${classes.margin_40}`}
+                    >
+                        <strong>
+                            Com amor e gratidão,
+                            <br/>
+                            HELANO MARIZ
+                        </strong>
+                    </Typography>
+
+                    <Button 
+                        color={'tertiary'}
+                        text={'Entrar para o grupo VIP'}
+                    />
+                </Container>
+            </Container>
+        </TemplateDefault>
+    )
+}
+
+export default Thank
