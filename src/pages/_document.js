55e1c37af99ca87a25c70802fd169b3a729d6458
Matheus Amon <amonmatheus757@gmail.com>
2024-03-03 16:24:21 -0300

    Adicionando gtm

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -24,6 +24,24 @@ class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <link rel="shortcut icon" href="/images/favicon.svg" type="image/png" />
+
+          <script dangerouslySetInnerHTML={{__html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://gtmserve.imersaohelanomariz.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W5FK3GFP');
+          `}} />        
+
+          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-345210624" />
+
+          <script dangerouslySetInnerHTML={{__html:`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-345210624');
+          `}} />
         </Head>
         <body>
           <Main />
