21a8bbe3f7dd230c3ab95a2b7a827d6011df14ab
Matheus Amon <amonmatheus757@gmail.com>
2024-03-07 13:56:05 -0300

    Voltando as tags scrpit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -28,7 +28,7 @@ class MyDocument extends Document {
 
           <meta name="facebook-domain-verification" content="umlywmh2jkgkjjuzk4kum2f0tigbpo" />
 
-          <Script async dangerouslySetInnerHTML={{__html: `
+          <script async dangerouslySetInnerHTML={{__html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -36,9 +36,9 @@ class MyDocument extends Document {
             })(window,document,'script','dataLayer','GTM-W5FK3GFP');
           `}} />        
 
-          <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-345210624" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-345210624" />
 
-          <Script async dangerouslySetInnerHTML={{__html:`
+          <script async dangerouslySetInnerHTML={{__html:`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
